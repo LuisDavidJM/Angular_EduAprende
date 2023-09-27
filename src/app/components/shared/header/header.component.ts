b7820167c4f1acd21d5e74d33809a258140edff3
Luis David Mac <luisdavidjma@gmail.com>
2023-09-27 17:30:15 -0600

    Login correcto

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private authService: LoginService, private router: Router) {}
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/login')
+  }
+
+  get userRole(): string | null {
+    return localStorage.getItem('role');
+  }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,36 +1,57 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, map, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, throwError } from 'rxjs';
 import { User } from './login';
 import { LoginRequest } from './loginRequest';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+const helper = new JwtHelperService();
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private loggedIn = new BehaviorSubject<boolean>(false);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.readToken();
+   }
 
   api: string = "http://localhost:3000";  
+
+   get isLogged(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+   }
 
   login(credentials:LoginRequest):Observable<User | void> {
     return this.http.post<User>(`${this.api}/auth/login`, credentials).pipe(
       map((response: User) => {
         //console.log("Res->", response)
         this.saveToken(response.token)
+        this.saveRole(response.role)
+        this.loggedIn.next(true);
+        return response;
       }),
       catchError(error => this.handleError(error))
     )
   }
   logout() { 
     localStorage.removeItem('token');
-    // set userIsLoggued = false
+    localStorage.removeItem('role')
+    this.loggedIn.next(false);
   }
   private readToken() {
-
+    const userToken = localStorage.getItem('token');
+    const isExpired = helper.isTokenExpired(userToken);
+    //console.log('isExpired->', isExpired);
+    isExpired ? this.logout() : this.loggedIn.next(true);
   }
   private saveToken(token: string) {
     localStorage.setItem('token', token);
+  }
+
+  private saveRole(role: string) {
+    localStorage.setItem('role', role);
   }
 
   private handleError(error: HttpErrorResponse): Observable<never> {
